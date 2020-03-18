20782ab731db5922080a2f7e377377f5208bcb2f
xu.deng <xu.deng@biz-united.com.cn>
2020-03-18 22:02:13 +0800

    通天塔

--- mydata/src/main/java/com/example/mydata/MydataApplication.java
+++ mydata/src/main/java/com/example/mydata/MydataApplication.java
@@ -4,7 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+*
+* */
 @SpringBootApplication
 @MapperScan(basePackages = "com.example.mydata.dao")
 public class MydataApplication {
